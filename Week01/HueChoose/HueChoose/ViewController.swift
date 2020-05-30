7e713812942862c534c6df5680f7701f849fdbb1
wbmatthews <billm@me.com>
2020-05-30 10:44:16 -0400

    Added IBOutlets and IBAction for sliders and set values for colour

--- Week01/HueChoose/HueChoose/ViewController.swift
+++ Week01/HueChoose/HueChoose/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var colourNameLabel: UILabel!
   @IBOutlet var mainView: UIView!
+  @IBOutlet weak var slider1: UISlider!
+  @IBOutlet weak var slider2: UISlider!
+  @IBOutlet weak var slider3: UISlider!
+  
+  var colourValues: [CGFloat] = [0,0,0]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,8 +31,7 @@ class ViewController: UIViewController {
     let setColourAction = UIAlertAction(title: "Set Colour", style: .default) { action in
       guard let textFields = setColourAlert.textFields, let colourNameField = textFields[0].text else { return }
       self.colourNameLabel.text = colourNameField
-      self.mainView.backgroundColor = .blue
-      // set colour here
+      self.mainView.backgroundColor = UIColor(red: self.colourValues[0], green: self.colourValues[1], blue: self.colourValues[2], alpha: 1.0)
     }
     let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
     
@@ -40,7 +44,25 @@ class ViewController: UIViewController {
   @IBAction func resetPressed() {
     colourNameLabel.text = "No colour set"
     mainView.backgroundColor = .systemBackground
+    
+    slider1.value = 0
+    slider2.value = 0
+    slider3.value = 0
+    
   }
   
+  @IBAction func sliderMoved(_ sender: UISlider) {
+    switch sender.tag {
+    case 100:
+      colourValues[0] = CGFloat(sender.value)
+      print(colourValues[0])
+      case 200:
+        colourValues[1] = CGFloat(sender.value)
+      case 300:
+        colourValues[2] = CGFloat(sender.value)
+    default:
+      return
+    }
+  }
 }
 
