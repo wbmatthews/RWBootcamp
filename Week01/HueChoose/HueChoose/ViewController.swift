bae05a713d082ab62a1239077810305649efdb5c
wbmatthews <billm@me.com>
2020-05-30 10:06:16 -0400

    Added IBOutlet and IBAction with alert and colour name setting for Set Colour button

--- Week01/HueChoose/HueChoose/ViewController.swift
+++ Week01/HueChoose/HueChoose/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var colourNameLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func setColourPressed() {
+    let setColourAlert = UIAlertController(title: "Name your colour", message: nil, preferredStyle: .alert)
+    setColourAlert.addTextField { (colourNameField) in
+      colourNameField.placeholder = "Enter colour name"
+    }
+    let setColourAction = UIAlertAction(title: "Set Colour", style: .default) { action in
+      guard let textFields = setColourAlert.textFields, let colourNameField = textFields[0].text else { return }
+      self.colourNameLabel.text = colourNameField
+      // set colour here
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    
+    setColourAlert.addAction(setColourAction)
+    setColourAlert.addAction(cancelAction)
+    
+    present(setColourAlert, animated: true)
 
+  }
+  
 }
 
