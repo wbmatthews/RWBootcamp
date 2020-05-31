fec31c8f8afd852bc4da00d7c8a95f79f3f3554b
wbmatthews <billm@me.com>
2020-05-31 09:35:08 -0400

    Added custom buttons

--- Week01/HueChoose/HueChoose/ViewController.swift
+++ Week01/HueChoose/HueChoose/ViewController.swift
@@ -28,11 +28,13 @@ class ViewController: UIViewController {
     setColourAlert.addTextField { (colourNameField) in
       colourNameField.placeholder = "Enter colour name"
     }
+    
     let setColourAction = UIAlertAction(title: "Set Colour", style: .default) { action in
       guard let textFields = setColourAlert.textFields, let colourNameField = textFields[0].text else { return }
       self.colourNameLabel.text = colourNameField
       self.mainView.backgroundColor = self.previewView.backgroundColor!
     }
+    
     let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
     
     setColourAlert.addAction(setColourAction)
@@ -46,7 +48,7 @@ class ViewController: UIViewController {
     colourNameLabel.text = "No colour set"
     mainView.backgroundColor = .systemBackground
     
-    _ = colourValues.map { _ in 0 }
+    colourValues = [0,0,0]
     _ = sliderLabels.map { $0.text = "0" }
     _ = sliders.map { $0.value = 0 }
     previewView.backgroundColor = .black
@@ -63,5 +65,6 @@ class ViewController: UIViewController {
     sliderLabels[index].text = ("\(Int(255 * colourValues[index]))")
     previewView.backgroundColor = UIColor(red: colourValues[0], green: colourValues[1], blue: colourValues[2], alpha: 1.0)
   }
+  
 }
 
