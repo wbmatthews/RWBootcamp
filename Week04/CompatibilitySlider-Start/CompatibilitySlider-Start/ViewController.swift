9f7ba491be6dd8125b41f114b8d328cc7522ae4b
wbmatthews <billm@me.com>
2020-06-22 21:13:22 -0400

    Fixed indents and adjusted the formula to be 0 to 4 so that there's a chance for 0% match.

--- Week04/CompatibilitySlider-Start/CompatibilitySlider-Start/ViewController.swift
+++ Week04/CompatibilitySlider-Start/CompatibilitySlider-Start/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var compatibilityItemLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var questionLabel: UILabel!
-
+  
+  @IBOutlet weak var compatibilityItemLabel: UILabel!
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var questionLabel: UILabel!
+  
   var compatibilityItems = [
     "Cats",
     "Dogs",
@@ -28,21 +28,21 @@ class ViewController: UIViewController {
     "Snow",
     "Politics"
   ]
-    var currentItemIndex = 0
-
-    var person1 = Person(id: 1, items: [:])
-    var person2 = Person(id: 2, items: [:])
-    var currentPerson: Person?
-
-    override func viewDidLoad() {
-      super.viewDidLoad()
-      currentPerson = person1
-      updateUI()
-    }
-
-    @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print(sender.value)
-    }
+  var currentItemIndex = 0
+  
+  var person1 = Person(id: 1, items: [:])
+  var person2 = Person(id: 2, items: [:])
+  var currentPerson: Person?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    currentPerson = person1
+    updateUI()
+  }
+  
+  @IBAction func sliderValueChanged(_ sender: UISlider) {
+    print(sender.value)
+  }
   
   @IBAction func didTapIcon(_ sender: UITapGestureRecognizer) {
     if let value = sender.view?.tag {
@@ -50,67 +50,69 @@ class ViewController: UIViewController {
     }
   }
   
-    @IBAction func didPressNextItemButton(_ sender: Any) {
-      currentPerson?.items[compatibilityItems[currentItemIndex]] = slider.value
-      currentItemIndex += 1
-      if currentItemIndex < compatibilityItems.count {
-        updateUI()
-      } else {
-        let alert: UIAlertController
-        if currentPerson == person1 {
-          
-          alert = UIAlertController(title: "All done!", message: "Thanks for your answers. Please pass the device to your potential soul mate so they can answer.", preferredStyle: .alert)
-          let action = UIAlertAction(title: "OK", style: .default) { alert in
-            self.currentPerson = self.person2
-            self.currentItemIndex = 0
-            self.updateUI()
-          }
-          alert.addAction(action)
-          
-        } else {
-          let compatibilityScore = calculateCompatibility()
-          print(compatibilityScore)
-          
-          alert = UIAlertController(title: "Results", message: "You two are \(compatibilityScore) compatible.", preferredStyle: .alert)
-          let action = UIAlertAction(title: "OK", style: .default) { alert in
-                      
-            self.person1 = Person(id: 1, items: [:])
-            self.person2 = Person(id: 2, items: [:])
-            self.currentPerson = self.person1
-            self.currentItemIndex = 0
-            self.updateUI()
-            
-          }
-          alert.addAction(action)
-        }
+  @IBAction func didPressNextItemButton(_ sender: Any) {
+    currentPerson?.items[compatibilityItems[currentItemIndex]] = slider.value
+    currentItemIndex += 1
+    if currentItemIndex < compatibilityItems.count {
+      updateUI()
+    } else {
+      let alert: UIAlertController
+      if currentPerson == person1 {
         
-        present(alert, animated: true, completion: nil)
+        alert = UIAlertController(title: "All done!", message: "Thanks for your answers. Please pass the device to your potential soul mate so they can answer.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { alert in
+          self.currentPerson = self.person2
+          self.currentItemIndex = 0
+          self.updateUI()
+        }
+        alert.addAction(action)
+        
+      } else {
+        let compatibilityScore = calculateCompatibility()
+        print(compatibilityScore)
+        
+        alert = UIAlertController(title: "Results", message: "You two are \(compatibilityScore) compatible.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { alert in
+          
+          self.person1 = Person(id: 1, items: [:])
+          self.person2 = Person(id: 2, items: [:])
+          self.currentPerson = self.person1
+          self.currentItemIndex = 0
+          self.updateUI()
+          
+        }
+        alert.addAction(action)
       }
+      
+      present(alert, animated: true, completion: nil)
     }
+  }
   
   func updateUI() {
     guard let currentPerson = currentPerson else { return }
     questionLabel.text = "User \(String(currentPerson.id)), what do you think about..."
     compatibilityItemLabel.text = compatibilityItems[currentItemIndex]
-    slider.value = 3
+    slider.value = 2.0
   }
-
-    func calculateCompatibility() -> String {
-        // If diff 0.0 is 100% and 5.0 is 0%, calculate match percentage
-        var percentagesForAllItems: [Double] = []
-
-        for (key, person1Rating) in person1.items {
-            let person2Rating = person2.items[key] ?? 0
-            let difference = abs(person1Rating - person2Rating)/5.0
-            percentagesForAllItems.append(Double(difference))
-        }
-
-        let sumOfAllPercentages = percentagesForAllItems.reduce(0, +)
-        let matchPercentage = sumOfAllPercentages/Double(compatibilityItems.count)
-        print(matchPercentage, "%")
-        let matchString = 100 - (matchPercentage * 100).rounded()
-        return "\(matchString)%"
+  
+  func calculateCompatibility() -> String {
+    // If diff 0.0 is 100% and 5.0 is 0%, calculate match percentage
+    // Updated the formula to run for 0 to 4 instead.
+    
+    var percentagesForAllItems: [Double] = []
+    
+    for (key, person1Rating) in person1.items {
+      let person2Rating = person2.items[key] ?? 0
+      let difference = abs(person1Rating - person2Rating)/4.0
+      percentagesForAllItems.append(Double(difference))
     }
-
+    
+    let sumOfAllPercentages = percentagesForAllItems.reduce(0, +)
+    let matchPercentage = sumOfAllPercentages/Double(compatibilityItems.count)
+    print(matchPercentage, "%")
+    let matchString = 100 - (matchPercentage * 100).rounded()
+    return "\(matchString)%"
+  }
+  
 }
 
