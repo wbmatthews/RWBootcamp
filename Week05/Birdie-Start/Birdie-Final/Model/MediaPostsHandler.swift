0ff3206fe47fbef924cfb0a1fffb4e17ae97f540
wbmatthews <billm@me.com>
2020-06-27 11:38:34 -0400

    Fixed the faux date stamps to be a little less disco.

--- Week05/Birdie-Start/Birdie-Final/Model/MediaPostsHandler.swift
+++ Week05/Birdie-Start/Birdie-Final/Model/MediaPostsHandler.swift
@@ -15,10 +15,10 @@ class MediaPostsHandler: NSObject {
     private override init() {}
 
     func getPosts() {
-        let imagePost1 = ImagePost(textBody: "I love debugging software!", userName: "Jay", timestamp: Date(timeIntervalSince1970: 10000), image: UIImage(named: "chop")!)
-        let imagePost2 = ImagePost(textBody: "Went to the Aquarium today :]", userName: "Audrey", timestamp: Date(timeIntervalSince1970: 30000), image: UIImage(named: "octopus")!)
-        let textPost1 = TextPost(textBody: "Hello World!", userName: "Bhagat", timestamp: Date(timeIntervalSince1970: 20000))
-        let textPost2 = TextPost(textBody: "This is my favorite social media app! This is my favorite social media app! This is my favorite social media app!", userName: "Jeff", timestamp: Date(timeIntervalSince1970: 40000))
+        let imagePost1 = ImagePost(textBody: "I love debugging software!", userName: "Jay", timestamp: Date(timeIntervalSinceNow: -123456), image: UIImage(named: "chop")!)
+        let imagePost2 = ImagePost(textBody: "Went to the Aquarium today :]", userName: "Audrey", timestamp: Date(timeIntervalSinceNow: -9876), image: UIImage(named: "octopus")!)
+        let textPost1 = TextPost(textBody: "Hello World!", userName: "Bhagat", timestamp: Date(timeIntervalSinceNow: -67890))
+        let textPost2 = TextPost(textBody: "This is my favorite social media app! This is my favorite social media app! This is my favorite social media app!", userName: "Jeff", timestamp: Date(timeIntervalSinceNow: -2345))
 
         mediaPosts = [imagePost1, imagePost2, textPost1, textPost2]
         mediaPosts = mediaPosts.sorted(by: { $0.timestamp > $1.timestamp })
