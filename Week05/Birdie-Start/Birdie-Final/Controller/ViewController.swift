f69e167a5025059057384df9e1e8de6207208d4b
wbmatthews <billm@me.com>
2020-06-28 10:42:39 -0400

    refactored createNewPost logic into function

--- Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -10,59 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
   
-    @IBOutlet weak var tableview: UITableView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      MediaPostsHandler.shared.getPosts()
-        setUpTableView()
-    }
-
-    func setUpTableView() {
-        // Set delegates, register custom cells, set up datasource, etc.
-      tableview.delegate = self
-      tableview.dataSource = self
-      let textPostCell = UINib(nibName: "TextPostCell", bundle: nil)
-      let imagePostCell = UINib(nibName: "ImagePostCell", bundle: nil)
-
-      tableview.register(textPostCell, forCellReuseIdentifier: "textPostCell")
-      tableview.register(imagePostCell, forCellReuseIdentifier: "imagePostCell")
-      
-    }
-
-  @IBAction func didPressCreateTextPostButton(_ sender: Any) {
-    
-    let newTextPostEntry = UIAlertController(title: "New Text Post", message: nil, preferredStyle: .alert)
-    
-    newTextPostEntry.addTextField { (userNameField) in
-      userNameField.placeholder = "User name"
-      userNameField.autocapitalizationType = .words
-    }
-    newTextPostEntry.addTextField { (bodyTextfield) in
-      bodyTextfield.placeholder = "Enter post here"
-    }
-    
-    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    let postAction = UIAlertAction(title: "Post", style: .default) { (alert) in
-      if let textFields = newTextPostEntry.textFields, let userName = textFields[0].text, let postBody = textFields[1].text {
-        MediaPostsHandler.shared.addTextPost(textPost: TextPost(textBody: postBody, userName: userName, timestamp: Date()))
-        self.tableview.reloadData()
-      }
-    }
-    newTextPostEntry.addAction(cancelAction)
-    newTextPostEntry.addAction(postAction)
-    
-    present(newTextPostEntry, animated: true, completion: nil)
+  @IBOutlet weak var tableview: UITableView!
+  var imageForPost: UIImage?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    MediaPostsHandler.shared.getPosts()
+    setUpTableView()
   }
-
-    @IBAction func didPressCreateImagePostButton(_ sender: Any) {
-      let picker = UIImagePickerController()
-      picker.delegate = self
+  
+  func setUpTableView() {
+    // Set delegates, register custom cells, set up datasource, etc.
+    tableview.delegate = self
+    tableview.dataSource = self
+    let textPostCell = UINib(nibName: "TextPostCell", bundle: nil)
+    let imagePostCell = UINib(nibName: "ImagePostCell", bundle: nil)
+    
+    tableview.register(textPostCell, forCellReuseIdentifier: "textPostCell")
+    tableview.register(imagePostCell, forCellReuseIdentifier: "imagePostCell")
+    
+  }
+  
+  @IBAction func didPressCreateTextPostButton(_ sender: Any) {
+    createNewPost()
+  }
+  
+  @IBAction func didPressCreateImagePostButton(_ sender: Any) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
       picker.sourceType = .photoLibrary
       picker.modalPresentationStyle = .fullScreen
       present(picker, animated: true, completion: nil)
     }
 
+  }
+  
+  func createNewPost() {
+
+    let newPostEntry = UIAlertController(title: imageForPost == nil ? "New Text Post" : "New Image Post", message: nil, preferredStyle: .alert)
+    
+    newPostEntry.addTextField { (userNameField) in
+      userNameField.placeholder = "User name"
+      userNameField.autocapitalizationType = .words
+    }
+    newPostEntry.addTextField { (bodyTextfield) in
+      bodyTextfield.placeholder = "Enter post here"
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    let postAction = UIAlertAction(title: "Post", style: .default) { (alert) in
+      if let textFields = newPostEntry.textFields, let userName = textFields[0].text, let postBody = textFields[1].text {
+        if let image = self.imageForPost {
+          MediaPostsHandler.shared.addImagePost(imagePost: ImagePost(textBody: postBody, userName: userName, timestamp: Date(), image: image))
+        } else {
+          MediaPostsHandler.shared.addTextPost(textPost: TextPost(textBody: postBody, userName: userName, timestamp: Date()))
+        }
+        self.tableview.reloadData()
+      }
+    }
+    newPostEntry.addAction(cancelAction)
+    newPostEntry.addAction(postAction)
+    
+    present(newPostEntry, animated: true, completion: nil)
+  }
+  
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -104,7 +116,10 @@ extension ViewController {
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-    
+    if let image = info[.originalImage] as? UIImage {
+      imageForPost = image
+    }
+    dismiss(animated: true, completion: nil)
   }
 }
 
