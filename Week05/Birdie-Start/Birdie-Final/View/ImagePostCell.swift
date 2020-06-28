5e4a4c0563fbd53495bb3cede8ec5233a93456ce
wbmatthews <billm@me.com>
2020-06-28 11:52:20 -0400

    Fixed text alignment for landscape mode body text for image post

--- Week05/Birdie-Start/Birdie-Final/View/ImagePostCell.swift
+++ Week05/Birdie-Start/Birdie-Final/View/ImagePostCell.swift
@@ -14,16 +14,6 @@ class ImagePostCell: UITableViewCell {
   @IBOutlet weak var timestampLabel: UILabel!
   @IBOutlet weak var textBodyLabel: UILabel!
   @IBOutlet weak var postImage: UIImageView!
-  
-  override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
