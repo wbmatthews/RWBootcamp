365347eefa50a93e121138be0d2f8f50fead08c7
wbmatthews <billm@me.com>
2020-07-29 16:20:21 -0400

    fixes audio bug

--- Week09/jQuiz/SoundManager.swift
+++ Week09/jQuiz/SoundManager.swift
@@ -15,50 +15,48 @@ class SoundManager: NSObject {
 
     private var player: AVAudioPlayer?
 
-    var isSoundEnabled: Bool? {
-        get {
-          if let isSoundEnabled = UserDefaults.standard.object(forKey: "sound") as? Bool {
-            return isSoundEnabled
-          } else {
-            return true
-          }
-        }
-      
-      set {
-        if let enablingSound = newValue {
-          if enablingSound {
-            let path = Bundle.main.path(forResource: "Jeopardy-theme-song", ofType: "mp3")!
-            let url = URL(fileURLWithPath: path)
-            do {
-              player = try AVAudioPlayer(contentsOf: url)
-              UserDefaults.standard.set(true, forKey: "sound")
-            } catch let error {
-              print("Audio error: \(error)")
-            }
-          } else {
-            player = nil
-            UserDefaults.standard.set(false, forKey: "sound")
-          }
-        }
+  var isSoundEnabled: Bool {
+    get {
+      if let isSoundEnabled = UserDefaults.standard.object(forKey: "sound") as? Bool {
+        return isSoundEnabled
+      } else {
+        return true
       }
+    }
+    set {
+      if newValue {
+        let path = Bundle.main.path(forResource: "Jeopardy-theme-song", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+          player = try AVAudioPlayer(contentsOf: url)
+          player?.play()
+          UserDefaults.standard.set(true, forKey: "sound")
+        } catch let error {
+          print("Audio error: \(error)")
+        }
+      } else {
+        player = nil
+        UserDefaults.standard.set(false, forKey: "sound")
+      }
+    }
   }
 
     func playSound() {
-      if let player = player {
-        if player.isPlaying {
-          player.pause()
-          player.currentTime = 0
+      if isSoundEnabled {
+        if let player = player {
+          if player.isPlaying {
+             player.pause()
+             player.currentTime = 0
+           }
+           player.play()
+        } else {
+          isSoundEnabled = true
         }
-        player.play()
       }
     }
 
     func toggleSoundPreference() {
-      if let _ = isSoundEnabled {
-        self.isSoundEnabled!.toggle()
-      } else {
-        self.isSoundEnabled = false
-      }
+      isSoundEnabled.toggle()
     }
 
 }
