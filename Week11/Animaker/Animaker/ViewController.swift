3ae89e8615dd02a9f0b1da486ad3b4ad67bcdd13
wbmatthews <billm@me.com>
2020-08-09 10:33:11 -0400

    Buttons deploy

--- Week11/Animaker/Animaker/ViewController.swift
+++ Week11/Animaker/Animaker/ViewController.swift
@@ -13,21 +13,38 @@ class ViewController: UIViewController {
   @IBOutlet weak var leftOfPlayConstraint: NSLayoutConstraint!
   @IBOutlet weak var rightOfPlayConstraint: NSLayoutConstraint!
   @IBOutlet weak var abovePlayConstraint: NSLayoutConstraint!
-  private let buttonSpacing = 20
+  private let buttonSpacingOpen: CGFloat = 20
+  private let buttonSpacingClosed: CGFloat = -60
+
+  private var isDeployed = false
+  private var isLaunched = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    toggleControls()
   }
   
-  private func openControls() {
+  private func toggleControls() {
+    leftOfPlayConstraint.constant = isDeployed ? buttonSpacingOpen : buttonSpacingClosed
+    rightOfPlayConstraint.constant = isDeployed ? buttonSpacingOpen : buttonSpacingClosed
+    abovePlayConstraint.constant = isDeployed ? buttonSpacingOpen : buttonSpacingClosed
     
+    if isLaunched {
+      UIView.animate(withDuration: 1/3, delay: 0, options: .curveEaseIn, animations: {
+        self.view.layoutIfNeeded()
+      }, completion: nil)
+    } else {
+      self.view.layoutIfNeeded()
+      isLaunched = true
+    }
+    isDeployed.toggle()
+  }
+
+  @IBAction func playPressed(_ sender: UIButton) {
+    toggleControls()
   }
   
-  private func closeControls() {
-    
+  @IBAction func controlPressed(_ sender: UIButton) {
   }
-
-
 }
 
