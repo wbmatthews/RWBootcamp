7a1d4d81d9e7f047e6681a1c79423ecb434b10bf
wbmatthews <billm@me.com>
2020-08-09 10:14:33 -0400

    Starting week 11

---
+++ Week11/Animaker/Animaker/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Animaker
+//
+//  Created by Bill Matthews on 2020-08-09.
+//  Copyright © 2020 Bill Matthews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var leftOfPlayConstraint: NSLayoutConstraint!
+  @IBOutlet weak var rightOfPlayConstraint: NSLayoutConstraint!
+  @IBOutlet weak var abovePlayConstraint: NSLayoutConstraint!
+  private let buttonSpacing = 20
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  private func openControls() {
+    
+  }
+  
+  private func closeControls() {
+    
+  }
+
+
+}
+
