5a795dfc4ca3836e2ef009cd193be65007d9d466
wbmatthews <billm@me.com>
2020-08-09 12:18:47 -0400

    Adds color and translation animations

--- Week11/Animaker/Animaker/ViewController.swift
+++ Week11/Animaker/Animaker/ViewController.swift
@@ -13,14 +13,22 @@ class ViewController: UIViewController {
   @IBOutlet weak var leftOfPlayConstraint: NSLayoutConstraint!
   @IBOutlet weak var rightOfPlayConstraint: NSLayoutConstraint!
   @IBOutlet weak var abovePlayConstraint: NSLayoutConstraint!
+  
+  @IBOutlet weak var animationObject: UIView!
+  
   private let buttonSpacingOpen: CGFloat = 20
   private let buttonSpacingClosed: CGFloat = -60
 
   private var isDeployed = true
   private var isLaunched = false
   
+  var animationScript: UIViewPropertyAnimator?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    animationObject.center = animationObject.superview!.center
+//    animationObject.center = CGPoint(x: animationObject.superview!.frame.size.width - (animationObject.frame.size.width/2), y: animationObject.superview!.frame.size.height - (animationObject.frame.size.height/2))
+    animationObject.translatesAutoresizingMaskIntoConstraints = false
     toggleControls()
   }
   
@@ -42,10 +50,60 @@ class ViewController: UIViewController {
   }
 
   @IBAction func playPressed(_ sender: UIButton) {
+    if animationScript == nil {
+      toggleControls()
+      return
+    }
+    
     toggleControls()
+    animationScript!.startAnimation()
+    print("Starting animation")
+    
+    
+    
   }
   
   @IBAction func controlPressed(_ sender: UIButton) {
+    if animationScript == nil {
+      animationScript = UIViewPropertyAnimator(duration: TimeInterval.random(in: 1...3), curve: .easeInOut)
+      
+      animationScript!.addCompletion({ (_) in
+        self.animationScript = nil
+        print("Animation complete")
+      })
+    }
+    
+    switch sender.currentTitle {
+    case "Color":
+      animationScript!.addAnimations {
+        self.animationObject.backgroundColor = UIColor.random
+      }
+      print("Adding Color step")
+    case "Translate":
+      animationScript!.addAnimations { [object = self.animationObject!] in
+        let container = object.superview!
+        let xRange = container.frame.size.width / 2 - 100
+        let yRange = container.frame.size.height / 2 - 200
+        let randomX = object.center.x + CGFloat.random(in: -xRange...xRange)
+        let randomY = object.center.y + CGFloat.random(in: -yRange...yRange)
+        self.animationObject.center = CGPoint(x: randomX, y: randomY)
+      }
+      print("Adding Translation step")
+    case "Scale":
+      print("Adding Scale step")
+    default:
+      print("Invalid Button")
+    }
   }
 }
 
+extension CGFloat {
+    static var random: CGFloat {
+           return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+extension UIColor {
+    static var random: UIColor {
+           return UIColor(red: .random, green: .random, blue: .random, alpha: 1.0)
+    }
+}
